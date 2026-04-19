9f62eb6ae1b76b51777f1b7c5f4f0b9e6dbe65c1
errphoenix <herrphoenix20@gmail.com>
2026-04-19 02:33:21 +0200

    physics: adjust rigid body simulation default parameters

--- physics/src/rigid.rs
+++ physics/src/rigid.rs
@@ -18,8 +18,8 @@ pub struct RigidBodyOptions {
 }
 
 pub const DEFAULT_GRAVITY: f32 = 9.807;
-pub const DEFAULT_DAMPING: f32 = 0.785;
-pub const DEFAULT_RESTITUTION: f32 = 0.2;
+pub const DEFAULT_DAMPING: f32 = 0.95;
+pub const DEFAULT_RESTITUTION: f32 = 0.01;
 pub const DEFAULT_FRICTION: f32 = 0.05;
 pub const DEFAULT_STATIC_VOLUMES_HASH_RESOLUTION: SpatialResolution = SpatialResolution::new(4.0);
 
