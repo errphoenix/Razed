a64fd6cfef71315dca555c36f72a0784239b8f55
errphoenix <herrphoenix20@gmail.com>
2026-08-16 21:15:41 +0200

    feat(core): create cubemap texture initialisation helpers

--- Razed/src/render/graphics.rs
+++ Razed/src/render/graphics.rs
@@ -1,3 +1,5 @@
+use ethel::assets::{AssetRegistry, Handle, HasMetadata, RawTexture, TextureMetadata};
+use janus::texture::{ImageFormat, ImageType, MipLevels, Tex, Texture};
 use rendrs::graphics::material::{MaterialGroup, MaterialLocationRegistry};
 
 use crate::assets::TextureRegistry;
@@ -75,6 +77,13 @@ rendrs::material_groups! {
             roughness    = asset(*materials::MATEX_DEV_ID_METAL048C_ROUGHNESS);
             displacement = asset(*materials::MATEX_DEV_ID_METAL048C_DISPLACEMENT);
         };
+        entry(materials::MATERIAL_DEV_NAME_METAL063) {
+            diffuse      = asset(*materials::MATEX_DEV_ID_METAL063_DIFFUSE);
+            normal       = asset(*materials::MATEX_DEV_ID_METAL063_NORMAL);
+            metallic     = asset(*materials::MATEX_DEV_ID_METAL063_METALLIC);
+            roughness    = asset(*materials::MATEX_DEV_ID_METAL063_ROUGHNESS);
+            displacement = asset(*materials::MATEX_DEV_ID_METAL063_DISPLACEMENT);
+        };
     }
 }
 
@@ -124,5 +133,81 @@ pub mod materials {
         dev.concrete012 => diffuse, normal, roughness, displacement;
         dev.road015c => diffuse, normal, occlusion, roughness, displacement;
         dev.metal048c => diffuse, normal, metallic, roughness, displacement;
+        dev.metal063 => diffuse, normal, metallic, roughness, displacement;
     }
+}
+
+pub fn pack_cubemap(
+    registry: &mut AssetRegistry<RawTexture, TextureMetadata>,
+    common_id: &str,
+) -> Texture {
+    let id_negx = janus::hash_string(&format!("{common_id}.negx"));
+    let id_negy = janus::hash_string(&format!("{common_id}.negy"));
+    let id_negz = janus::hash_string(&format!("{common_id}.negz"));
+    let id_posx = janus::hash_string(&format!("{common_id}.posx"));
+    let id_posy = janus::hash_string(&format!("{common_id}.posy"));
+    let id_posz = janus::hash_string(&format!("{common_id}.posz"));
+
+    let negx = registry.unregister(id_negx).unwrap();
+    let negy = registry.unregister(id_negy).unwrap();
+    let negz = registry.unregister(id_negz).unwrap();
+    let posx = registry.unregister(id_posx).unwrap();
+    let posy = registry.unregister(id_posy).unwrap();
+    let posz = registry.unregister(id_posz).unwrap();
+
+    pack_cubemap_faces(negx, negy, negz, posx, posy, posz)
+}
+
+type TextureAssetHandle = Handle<RawTexture, TextureMetadata>;
+
+pub fn pack_cubemap_faces(
+    negx: TextureAssetHandle,
+    negy: TextureAssetHandle,
+    negz: TextureAssetHandle,
+    posx: TextureAssetHandle,
+    posy: TextureAssetHandle,
+    posz: TextureAssetHandle,
+) -> Texture {
+    {
+        let negx = negx.metadata();
+        let negy = negy.metadata();
+        let negz = negz.metadata();
+        let posx = posx.metadata();
+        let posy = posy.metadata();
+        let posz = posz.metadata();
+        assert!(
+            negx.size == negy.size
+                && negy.size == negz.size
+                && negz.size == posx.size
+                && posx.size == posy.size
+                && posy.size == posz.size
+        );
+    }
+
+    let (w, h) = negx
+        .metadata()
+        .size
+        .expect("texture asset and its metadata must be properly initialised");
+    let cubemap = Texture::new_cubemap(
+        w as i32,
+        h as i32,
+        MipLevels::default(),
+        ImageType::Bits8,
+        ImageFormat::Rgb,
+    );
+
+    let faces = [
+        negx.raw_resource().unwrap().0.as_bytes(),
+        negy.raw_resource().unwrap().0.as_bytes(),
+        negz.raw_resource().unwrap().0.as_bytes(),
+        posx.raw_resource().unwrap().0.as_bytes(),
+        posy.raw_resource().unwrap().0.as_bytes(),
+        posz.raw_resource().unwrap().0.as_bytes(),
+    ];
+
+    for i in 0..6 {
+        cubemap.upload_layer(0, 0, 0, i, w as i32, h as i32, faces[i as usize]);
+    }
+
+    cubemap
 }
